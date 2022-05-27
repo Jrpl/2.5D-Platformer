2609bb93c817a9e44128dffbbb4728bc18f8a774
Justin Pfeifler <jrpl101998@gmail.com>
2022-05-27 05:04:39 -0500

    Add coin script

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{    
+    private Player _player;
+    [SerializeField]
+    private AudioClip _coinSound;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player _player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+        if (!_player)
+        {
+            Debug.LogError("Failed to find Game Object with tag: Player");
+        }
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.tag == "Player") {
+            _player.AddCoin();
+            this.GetComponent<SphereCollider>().enabled = false;
+            Debug.Log("Play sound!");
+            AudioSource.PlayClipAtPoint(_coinSound, transform.position);
+            Destroy(this.gameObject);
+        }
+    }
+}
