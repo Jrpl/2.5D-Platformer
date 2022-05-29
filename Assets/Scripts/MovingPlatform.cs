c0b3c16ce0f1a2d0765540b1453cce8c94d487a5
Justin Pfeifler <jrpl101998@gmail.com>
2022-05-29 02:51:41 -0500

    Add moving platform script

---
+++ Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _pointA;
+    [SerializeField]
+    private GameObject _pointB;
+    [SerializeField]
+    private float _speed = 3f;
+    private Vector3 _direction;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _pointA = GameObject.Find("Point_A");
+        if (!_pointA) {
+            Debug.LogError("Failed to find Game Object: Point_A");
+        }
+
+        _pointB = GameObject.Find("Point_B");
+        if (!_pointB) {
+            Debug.LogError("Failed to find Game Object: Point_B");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move() {
+        if (transform.position.x <= _pointA.transform.position.x) {
+            _direction = new Vector3(_speed, 0, 0) * Time.deltaTime;
+        } 
+        if (transform.position.x >= _pointB.transform.position.x) {
+            _direction = new Vector3(-_speed, 0, 0) * Time.deltaTime;
+        }
+        
+        transform.Translate(_direction);
+    }
+}
