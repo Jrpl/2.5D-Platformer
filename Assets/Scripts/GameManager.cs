392d8cb960b00422d8340a24593041e2a85f767f
Justin Pfeifler <jrpl101998@gmail.com>
2022-05-31 04:24:32 -0500

    Add game manager script
    Add game over check
    Add restart input check
    Add pause input check

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    private bool _gameOver = false;
+    private bool _isPaused = false;
+    [SerializeField]
+    private UIManager _uiManager;
+
+    void Start()
+    {
+        _uiManager = GameObject.Find("UIManager").GetComponent<UIManager>();
+        if (!_uiManager)
+        {
+            Debug.LogError("Failed to find Game Object: UIManager");
+        }
+    }
+
+    void Update()
+    {
+        if (_gameOver)
+        {
+            if ((Input.GetKeyDown(KeyCode.R)))
+            {
+                SceneManager.LoadScene("SampleScene");
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (_isPaused)
+            {
+                _isPaused = false;
+                _uiManager.SetPauseScreen(_isPaused);
+                Time.timeScale = 1;
+            }
+            else
+            {
+                _isPaused = true;
+                _uiManager.SetPauseScreen(_isPaused);
+                Time.timeScale = 0;
+            }
+        }
+    }
+
+    public void setGameOver()
+    {
+        _gameOver = true;
+    }
+}
