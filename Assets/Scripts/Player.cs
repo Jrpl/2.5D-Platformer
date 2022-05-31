183d3df0e0d4d0429a4cfc841959c6c5b0b67815
Justin Pfeifler <jrpl101998@gmail.com>
2022-05-31 04:22:18 -0500

    Add health and health display
    Add pause screen
    Add game over screen

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Player : MonoBehaviour
 {
@@ -17,27 +15,35 @@ public class Player : MonoBehaviour
     private int _coins = 0;
     private float _yVelocity;
     private int _jumpCount;
-
+    private int _health = 3;
+    private Vector3 _spawn;
     private UIManager _uiManager;
 
-    // Start is called before the first frame update
     void Start()
     {
         _controller = GetComponent<CharacterController>();
-        if (!_controller) {
+        if (!_controller)
+        {
             Debug.LogError("Failed to find Character Controller");
         }
 
         _uiManager = GameObject.Find("UIManager").GetComponent<UIManager>();
-        if (!_uiManager) {
+        if (!_uiManager)
+        {
             Debug.LogError("Failed to find Game Object: UIManager");
+        }
+
+        _spawn = transform.position;
+        if (_spawn == new Vector3())
+        {
+            Debug.LogError("Failed to find transform.position of Game Object: Player");
         }
     }
 
-    // Update is called once per frame
     void Update()
     {
         Movement();
+        CheckFallDamage();
     }
 
     void Movement()
@@ -46,18 +52,24 @@ public class Player : MonoBehaviour
         float xInput = Input.GetAxis("Horizontal");
         Vector3 direction = new Vector3(xInput, 0, 0);
         Vector3 velocity = direction * _speed;
-        
+
         // Check if grounded, if not apply gravity
-        if(_controller.isGrounded){
+        if (_controller.isGrounded)
+        {
             _jumpCount = 0;
 
-            if(Input.GetKeyDown(KeyCode.Space)) {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
                 _jumpCount++;
                 _yVelocity = _jumpHeight;
             }
-        } else {
-            if(_jumpCount < _maxJumpCount) {
-                if(Input.GetKeyDown(KeyCode.Space)) {
+        }
+        else
+        {
+            if (_jumpCount < _maxJumpCount)
+            {
+                if (Input.GetKeyDown(KeyCode.Space))
+                {
                     _jumpCount++;
                     _yVelocity += _jumpHeight;
                 }
@@ -70,8 +82,24 @@ public class Player : MonoBehaviour
         _controller.Move(velocity * Time.deltaTime);
     }
 
-    public void AddCoin() {
+    public void AddCoin()
+    {
         _coins++;
         _uiManager.UpdateCoins(_coins);
+    }
+
+    public void CheckFallDamage()
+    {
+        if (transform.position.y < -3)
+        {
+            _health--;
+            gameObject.SetActive(false);
+            _uiManager.UpdateHealthDisplay(_health);
+            if (_health > 0)
+            {
+                transform.position = _spawn;
+                gameObject.SetActive(true);
+            }
+        }
     }
 }
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,22 +1,94 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
+    // Coins
     [SerializeField]
     private Text _coinText;
-    // Start is called before the first frame update
+
+    // Game Over
+    [SerializeField]
+    private GameObject _gameOverScreen;
+    [SerializeField]
+    private Text _gameOverText;
+
+    // Pause
+    [SerializeField]
+    private GameObject _pauseScreen;
+
+    // Health
+    [SerializeField]
+    private GameObject _heart1;
+    [SerializeField]
+    private GameObject _heart2;
+    [SerializeField]
+    private GameObject _heart3;
+
+    // Scripts
+    private GameManager _gameManager;
+
     void Start()
     {
         _coinText.text = "Coins: " + 0;
+        _heart1.SetActive(true);
+        _heart2.SetActive(true);
+        _heart3.SetActive(true);
+        _gameOverScreen.gameObject.SetActive(false);
+        _pauseScreen.gameObject.SetActive(false);
+        _gameManager = GameObject.Find("Game_Manager").GetComponent<GameManager>();
+        if (!_gameManager)
+        {
+            Debug.LogError("Failed to find Game Object: Game_Manager");
+        }
     }
 
-    // Update is called once per frame
     public void UpdateCoins(int coins)
     {
         _coinText.text = "Coins: " + coins;
+    }
+
+    public void UpdateHealthDisplay(int currentHealth)
+    {
+        switch (currentHealth)
+        {
+            case 2:
+                _heart3.SetActive(false);
+                break;
+            case 1:
+                _heart2.SetActive(false);
+                break;
+            case 0:
+                _heart1.SetActive(false);
+                GameOverSequence();
+                break;
+            default:
+                Debug.LogError("Failed to update health display. Current Health: " + currentHealth);
+                break;
+        }
+    }
+
+    void GameOverSequence()
+    {
+        _gameManager.setGameOver();
+        _gameOverScreen.SetActive(true);
+        StartCoroutine(GameOverFlickerRoutine());
+    }
+
+    public void SetPauseScreen(bool status)
+    {
+        _pauseScreen.SetActive(status);
+    }
+
+    IEnumerator GameOverFlickerRoutine()
+    {
+        while (true)
+        {
+            _gameOverText.gameObject.SetActive(true);
+            yield return new WaitForSeconds(0.5f);
+            _gameOverText.gameObject.SetActive(false);
+            yield return new WaitForSeconds(0.5f);
+        }
     }
 }
